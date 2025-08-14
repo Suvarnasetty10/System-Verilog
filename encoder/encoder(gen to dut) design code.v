4c6e57fb586af21e062b606302c7fb980edee59f
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-14 11:11:51 +0530

    encoder(gen to dut) design code.v

---
+++ encoder/encoder(gen to dut) design code.v
@@ -0,0 +1,19 @@
+// Code your design here
+module encoder(s,y);
+  input[3:0]s;
+  output reg [1:0]y;
+  always@(*)begin
+    case(s)
+      4'b0001:y=2'b00;
+      4'b0010:y=2'b01;
+      4'b0100:y=2'b10;
+      4'b1000:y=2'b11;
+      default:y=2'b00;
+    endcase
+  end
+endmodule
+
+interface inter;
+  logic[3:0] s;
+  logic[1:0] y;
+endinterface
