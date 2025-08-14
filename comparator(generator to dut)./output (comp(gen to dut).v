1616f99cf3711f9e5ac545d60aacc0ef70096d4a
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-14 14:57:03 +0530

    output (comp(gen to dut).v

---
+++ comparator(generator to dut)./output (comp(gen to dut).v
@@ -0,0 +1,10 @@
+# KERNEL: generated values a=0,b=1
+# KERNEL: generated values a=1,b=1
+# KERNEL: generated values a=1,b=1
+# KERNEL: generated values a=1,b=0
+# KERNEL: generated values a=1,b=1
+# KERNEL: driver values a=0,b=1, a_equal_b=0,a_less_b=1,a_greater_b=0
+# KERNEL: driver values a=1,b=1, a_equal_b=1,a_less_b=0,a_greater_b=0
+# KERNEL: driver values a=1,b=1, a_equal_b=1,a_less_b=0,a_greater_b=0
+# KERNEL: driver values a=1,b=0, a_equal_b=0,a_less_b=0,a_greater_b=1
+# KERNEL: driver values a=1,b=1, a_equal_b=1,a_less_b=0,a_greater_b=0
