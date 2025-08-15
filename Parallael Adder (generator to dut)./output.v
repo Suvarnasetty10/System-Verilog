aeaabc16823aa13cb4d1e7f96ad0fe5ae0b4e633
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-15 11:37:09 +0530

    Create output.v

---
+++ Parallael Adder (generator to dut)./output.v
@@ -0,0 +1,12 @@
+# KERNEL:  generator _ a=0100,b=0001,cin=1
+# KERNEL:  generator _ a=0011,b=1101,cin=1
+# KERNEL:  generator _ a=0101,b=0010,cin=1
+# KERNEL:  generator _ a=1101,b=0110,cin=1
+# KERNEL:  generator _ a=1101,b=1100,cin=1
+# KERNEL:  generator _ a=0110,b=0101,cin=0
+# KERNEL:  driver_a=0100,b=0001,cin=1,s=0110,c=0001
+# KERNEL:  driver_a=0011,b=1101,cin=1,s=0001,c=1111
+# KERNEL:  driver_a=0101,b=0010,cin=1,s=1000,c=0111
+# KERNEL:  driver_a=1101,b=0110,cin=1,s=0100,c=1111
+# KERNEL:  driver_a=1101,b=1100,cin=1,s=1010,c=1101
+# KERNEL:  driver_a=0110,b=0101,cin=0,s=1011,c=0100
